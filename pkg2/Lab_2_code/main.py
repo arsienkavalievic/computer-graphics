4cf3b25fab2fc0d0a2abf2d2cb9f428ded8f2e5b
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-11-21 19:27:30 +0300

    pkg2 build

--- pkg2/Lab_2_code/main.py
+++ pkg2/Lab_2_code/main.py
@@ -1,26 +1,77 @@
-import cv2 as cv
-import numpy as np
-import matplotlib as plt
 from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk
+from PIL import Image
 
 def select_image():
-	# grab a reference to the image panels
-	global panelA, panelB
-	# open a file chooser dialog and allow the user to select an input
-	# image
-	path = tkFileDialog.askopenfilename()
+	global panelA, name_label, size_label, resolution_label, depth_label, compression_label
+
+	path = filedialog.askopenfilename(filetypes=(("PNG", "*.png"), ("JPG", "*.jpg"), ("GIF", "*.gif"), ("TIF", "*.tif"), ("BMP", "*.bmp"), ("PCX", "*.pcx")))
+
+	if len(path) > 0:
+		image = Image.open(path)
+
+		name = path.split("/")[-1]
+		width, height = image.size
+		resolution = image.info.get("dpi")
+		depth = len(image.mode) * 8
+		compression = image.info.get("compression")
+
+		name_text = "Name: " + str(name)
+		size_text = "Size: " + str(width) + "x" + str(height) + "p"
+		if resolution:
+			resolution_text = "Resolution: " + str(resolution[0]) + "dpi"
+		else:
+			resolution_text = "Resolution not set."
+		depth_text = "Depth: " + str(depth) + " bit"
+		if compression:
+			compression_text = "Compression: " + str(compression)
+		else:
+			compression_text = "Compression not set."
 
 
-img1 = cv.imread("examples/kitties.jpg")
-img2 = cv.imread("examples/minecraft.png")
-img3 = cv.imread("examples/saw.jpg")
-img4 = cv.imread("examples/cats.gif")
+		height = (height / width) * 400
+		width = 400
+		image = ImageTk.PhotoImage(image.resize((int(width), int(height))))
 
+		if panelA is None:
+			panelA = Label(image=image)
+			panelA.image = image
+			panelA.pack(side="left", padx=10, pady=10)
+
+			name_label = Label(window, text=name_text)
+			name_label.pack(side="top", padx=10, pady=5)
+
+			size_label = Label(window, text=size_text)
+			size_label.pack(side="top", padx=10, pady=5)
+
+			resolution_label = Label(window, text=resolution_text)
+			resolution_label.pack(side="top", padx=10, pady=5)
+
+			depth_label = Label(window, text=depth_text)
+			depth_label.pack(side="top", padx=10, pady=5)
+
+			compression_label = Label(window, text=compression_text)
+			compression_label.pack(side="top", padx=10, pady=5)
+		else:
+			panelA.configure(image=image)
+			name_label.configure(text=name_text)
+			size_label.configure(text=size_text)
+			resolution_label.configure(text=resolution_text)
+			depth_label.configure(text=depth_text)
+			compression_label.configure(text=compression_text)
+
+			panelA.image = image
+			name_label.text = name_text
+			size_label.text = size_text
+			resolution_label.text = resolution_text
+			depth_label.text = depth_text
+			compression_label.text = compression_text
 
 window = Tk()
+panelA = None
+panelB = None
 window.title("Images")
+btn = Button(window, text="Select an image", command=select_image)
+btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
 window.mainloop()
-btn = Button(root, text="Select an image", command=select_image)
-
-cv.imshow("Display window", img1)
-k = cv.waitKey(0)

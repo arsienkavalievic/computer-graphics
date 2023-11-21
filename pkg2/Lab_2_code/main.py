aacb34562d1658328f14185649fcd90dcaf5c197
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-11-21 08:49:49 +0300

    pkg1 final

--- pkg2/Lab_2_code/main.py
+++ pkg2/Lab_2_code/main.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 import numpy as np
+import matplotlib as plt
 
-img = cv.imread("path/to/image")
+img = cv.imread("examples/kitties.jpg")
 
 cv.imshow("Display window", img)
 k = cv.waitKey(0) # Wait for a keystroke in the window

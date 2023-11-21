065039950952d98da17b35d383c96cc63acbae8d
Arsien Kavalievic <arsienkavalievic@gmail.com>
2023-11-21 11:34:36 +0300

    pkg2 and pkg3

--- pkg2/Lab_2_code/main.py
+++ pkg2/Lab_2_code/main.py
@@ -1,8 +1,26 @@
 import cv2 as cv
 import numpy as np
 import matplotlib as plt
+from tkinter import *
 
-img = cv.imread("examples/kitties.jpg")
+def select_image():
+	# grab a reference to the image panels
+	global panelA, panelB
+	# open a file chooser dialog and allow the user to select an input
+	# image
+	path = tkFileDialog.askopenfilename()
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0) # Wait for a keystroke in the window
+
+img1 = cv.imread("examples/kitties.jpg")
+img2 = cv.imread("examples/minecraft.png")
+img3 = cv.imread("examples/saw.jpg")
+img4 = cv.imread("examples/cats.gif")
+
+
+window = Tk()
+window.title("Images")
+window.mainloop()
+btn = Button(root, text="Select an image", command=select_image)
+
+cv.imshow("Display window", img1)
+k = cv.waitKey(0)
